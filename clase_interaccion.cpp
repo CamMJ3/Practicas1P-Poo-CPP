c563ae395758d0b59690a9610dae538c593104c7
Camila <camila.andreamtz@gmail.com>
2026-09-06 20:01:04 -0600

    First commit of the project that includes the main files (no code yet) and a .gitignore

---
+++ clase_interaccion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/** 
+ * @file 
+ * @brief Implementación de de los constructores y métodos de la plantilla Grafo.
+ */
+
+
+/***********************************************************************/
+
+/** 
+ * @brief 
+ * @details
+ * @param 
+ */
+
+
+/***********************************************************************/
