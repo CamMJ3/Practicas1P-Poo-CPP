fc6bdeb914a77e15cd84ccbfb55c860e3fbe008e
Camila <camila.andreamtz@gmail.com>
2026-09-09 19:28:55 -0600

    Created the class Estudiante with five properties, a constructor, and three methods (no code yet)

---
+++ clase_estudiante.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+/** 
+ * @file 
+ * @brief Funciones (métodos) disponibles de la calculadora. Se utiliza un retorno de tipo double en sus
+ * métodos para manejar números decimales eficientemente. Debido a que funciona a partir de dos números,
+ * se establecen los parámetros "num1" y "num2".
+ */
+
+class Estudiante
+{
+    private:
+        string nombre;
+        string carrera;
+        int promedio;
+        int matricula;
+        int materias_cursando;
+    public:
+        Estudiante(string nombre, string carrera, int promedio, int matricula, int materias_cursando);
+        void obtener_datos();
+        bool aprobado();
+        int obtener_matricula();
+};
+
+int menu(); // Menú del programa que maneja el input (aportación) del usuario
+
+/***********************************************************************/
+
+
+
+/***********************************************************************/
+
+int main()
+{
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+    while (true)
+	{
+		switch (menu())
+		{
+        case 1:
+        {
+            double num1, num2;
+            cout <<  "\nIngrese el primer número: "; 
+            cin >> num1;
+
+            cout <<  "\nIngrese el segundo número: "; 
+            cin >> num2;
+
+            cout << "\nLa suma de " << num1 << " + " << num2 << " es : " << sumar(num1, num2) << endl;
+            break;
+        }
+		case 2:
+        {
+            double num1, num2;
+            cout <<  "\nIngrese el primer número: "; 
+            cin >> num1;
+
+            cout <<  "\nIngrese el segundo número: "; 
+            cin >> num2;
+
+            cout << "\nLa resta de " << num1 << " - " << num2 << " es : " << restar(num1, num2) << endl;
+            break;
+        }
+		case 3:
+        {
+            double num1, num2;
+            cout <<  "\nIngrese el primer número: "; 
+            cin >> num1;
+
+            cout <<  "\nIngrese el segundo número: "; 
+            cin >> num2;
+
+            cout << "\nLa multiplicación de " << num1 << " * " << num2 << " es : " << multiplicar(num1, num2) << endl;
+            break;
+        }
+        case 4:
+        {
+            double num1, num2;
+            cout <<  "\nIngrese el primer número: "; 
+            cin >> num1;
+
+            cout <<  "\nIngrese el segundo número: "; 
+            cin >> num2;
+
+            cout << "\nLa división de " << num1 << " / " << num2 << " es : " << dividir(num1, num2) << endl;
+            break;
+        }
+        case 5:
+        {
+			cout << "\nSaliendo del programa...";
+			exit(0);
+			break;
+        }
+        }
+    }
+}
+
+/***********************************************************************/
+
+/** 
+ * @brief Menú del programa que maneja el input (aportación) del usuario.
+ *
+ * @details Devuelve una opción a la función main().
+ */
+
+int menu()
+{
+    int opcion;
+	do
+	{
+		cout << "\n\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ ¡ MENÚ ! -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_";
+        cout << "\n\n-------> Calculadora.\n";
+        cout << "1) Sumar dos números.\n";
+		cout << "2) Restar dos números.\n";
+		cout << "3) Multiplicar dos números.\n";
+        cout << "4) Dividir dos números.\n";
+        cout << "5) Salir del programa.\n";
+		cout << "\n--> Seleccione una opción: ";
+		cin >> opcion;
+	} while (opcion < 1 || opcion > 5);
+	return opcion;
+}
